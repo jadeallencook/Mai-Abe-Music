adb3ce3767c9f6dcb04d4cdceda7f3651e453a1e
Jade Allen Cook <jadeallencook@gmail.com>
2018-12-21 02:30:24 -0700

    🛠 Initial boilerplate

---
+++ build/precache-manifest.7bf89e62ba40ed7e17d3dcb0be4dae3c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "970590077b74db867b2c",
+    "url": "/static/js/main.97059007.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "970590077b74db867b2c",
+    "url": "/static/css/main.2588d1a2.chunk.css"
+  },
+  {
+    "revision": "2318e87e368520e7bf6de16920255062",
+    "url": "/index.html"
+  }
+];
